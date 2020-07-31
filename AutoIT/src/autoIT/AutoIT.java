16589f609960f0fa20648e88e3ce67ac3e56798b
Prasanna304 <vpk304@gmail.com>
2020-07-31 17:21:55 +0530

    First Commit

--- AutoIT/src/autoIT/AutoIT.java
+++ AutoIT/src/autoIT/AutoIT.java
@@ -3,8 +3,16 @@ package autoIT;
 public class AutoIT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		File f = new File("");
+		String Path = f.getAbsolutePath();
+		System.setProperty("webdriver.chrome.driver",Path+"\\Driver\\chromedriver");
+		WebDriver wd = new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("http://www.tinyupload.com/");
+		WebElement upload = wd.findElement(By.xpath("/html/body/table/tbody/tr[4]/td/table/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/input[2]"));
+		upload.click();
+		Thread.sleep(2000);
+		Runtime.getRuntime().exec(Path+"\\AutoITSoftware\\AutoITTest.exe");
 	}
-
 }
